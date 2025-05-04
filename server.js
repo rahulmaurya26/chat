d426c2e715302d2ae21c36c7cb19d4787d46378c
Rahul Maurya <rahulmaurya6055@gmail.com>
2025-05-04 12:46:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname)); // Serve HTML from current folder
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
